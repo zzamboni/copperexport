f918b93fc5367d432bf3c0c4b797918dc5a9e918
Diego Zamboni <diego@zzamboni.org>
2006-01-16 00:04:40 +0000

    Updated documentation and ChangeLog for 0.4.0 release.

--- scripts/mkimage.sh
+++ scripts/mkimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMGDIR=build/distimage
-SOURCE_FILES="build/CopperExport.pkg build/CopperExport.pkg/Contents/Resources/ReadMe.rtf"
+SOURCE_FILES="build/CopperExport.pkg build/CopperExport.pkg/Contents/Resources/ReadMe.rtf ChangeLog cpg-patches"
 
 FINALIMG=`echo 'build/CopperExport-[[version]].dmg' | ./scripts/subst_keywords.pl`
 
@@ -12,3 +12,4 @@ for i in ${SOURCE_FILES}; do
 done
 hdiutil create -srcfolder ${IMGDIR} -format UDZO -imagekey zlib-level=9 -volname CopperExport ${FINALIMG}
 rm -rf ${IMGDIR}
+open build/
