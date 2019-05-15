432aa913742c187540b3cd9cecf74c96f2e78e84
Diego Zamboni <diego@zzamboni.org>
2019-05-15 13:15:06 +0200

    Committing latest changes

--- scripts/mkpackage.sh
+++ scripts/mkpackage.sh
@@ -7,6 +7,6 @@ for i in package/Install_resources/*; do
 	./scripts/subst_keywords.pl $i ${INSTRES}/`basename $i`
 done
 ./scripts/subst_keywords.pl package/Info.plist build/Info.plist
-/Developer/Tools/packagemaker -build -v -ds -p build/CopperExport.pkg -f build/Package_contents -r ${INSTRES} -i build/Info.plist -d package/Description.plist
+/Developer/usr/bin/packagemaker -build -v -ds -p build/CopperExport.pkg -f build/Package_contents -r ${INSTRES} -i build/Info.plist -d package/Description.plist
 cp package/TokenDefinitions.plist package/IFRequirement.strings build/CopperExport.pkg/Contents/Resources/
 rm -rf build/Package_contents ${INSTRES} build/Info.plist
